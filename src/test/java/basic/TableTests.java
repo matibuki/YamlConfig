3a6001e20222b4fc57ffe54e68cde6d1b940f39b
Mateusz <mateusz.bukowski.ext@iceservices.com>
2022-09-07 14:09:57 +0200

    update#3

--- src/test/java/basic/TableTests.java
+++ src/test/java/basic/TableTests.java
@@ -1,10 +1,32 @@
 package basic;
 
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
-public class TableTests extends TestBase{
+import java.util.ArrayList;
+import java.util.List;
+
+public class TableTests extends TestBase {
     @Test
     public void tableTest() {
+        getDriver().get("https://seleniumui.moderntester.pl/table.php");
+        WebElement table = findElementByCss(".table");
+        List<WebElement> rows = table.findElements(By.tagName("tr"));
+        List<WebElement> columns = table.findElements(By.tagName("td"));
+        List<String> value = new ArrayList<>();
+
+        System.out.println(rows.size());
+
+//        for (WebElement data : rows) {
+        for (int i = 0; i < columns.size(); i++) {
+            System.out.println(columns.get(i).getText());
+            value.add(columns.get(i).getText());
+
+        }
+        System.out.println("test");
+        System.out.println("test");
+        System.out.println("test");
 
     }
 }
---
+++ src/test/java/basic/WindowsTests.java
@@ -0,0 +1,13 @@
+package basic;
+
+import com.google.common.collect.Table;
+import org.junit.jupiter.api.Test;
+
+public class WindowsTests extends TestBase{
+    TableTests newTableTestObject;
+    @Test
+    public void windowTest() {
+
+        newTableTestObject.tableTest();
+    }
+}
